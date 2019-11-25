b983910dc56440d63b977622d6f9e0e7425e018b
Jonathan Koblyuck <jkobyluck@my365.bellevue.edu>
2019-11-25 17:08:03 -0500

    Updated file name for Exercise 1.3 assignment to correct file-naming style

---
+++ week-1/kobyluck-exercise-1.3.js
@@ -0,0 +1,13 @@
+/*======================================
+; Title: Exercise 1.3
+; Author: Professor Krasso
+; Date; 25 November 2019
+; Modified By: Jonathan Kobyluck
+; Description: This is the "Hello World"
+; exercise in WEB231.
+; ======================================
+*/
+
+
+
+console.log("Hello World, my name is Johnny Kobyluck");
