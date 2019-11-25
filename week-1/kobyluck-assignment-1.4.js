fa24c737d8c43dfd8bd9f4cd3b7c003c3e155905
Jonathan Koblyuck <jkobyluck@my365.bellevue.edu>
2019-11-25 17:51:35 -0500

    Initial commit for assignment 1.4

---
+++ week-1/kobyluck-assignment-1.4.js
@@ -0,0 +1,41 @@
+/*======================================
+; Title: Exercise 1.4
+; Author: Professor Krasso
+; Date; 25 November 2019
+; Modified By: Jonathan Kobyluck
+; Description: Assigning variables and
+; outputting them to display in console
+; ======================================
+*/
+
+//start program
+
+
+/*
+    Expected output:
+
+    First name
+    Last Name
+    Age
+    Street
+    City
+    Zip code
+*/
+
+//variable assignment
+var firstName = "Johnny";
+var lastName = "Kobyluck";
+var age = "29";
+var street = "123 JavaScript Ln";
+var city = "Codeville";
+var zipCode = "54321";
+
+//output
+console.log(firstName);
+console.log(lastName);
+console.log(age);
+console.log(street);
+console.log(city);
+console.log(zipCode);
+
+//end program
