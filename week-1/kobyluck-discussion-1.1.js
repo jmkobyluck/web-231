feb30b1740b683b53452e2be6319826fc274e9c0
Jonathan Koblyuck <jkobyluck@my365.bellevue.edu>
2019-11-27 21:46:14 -0500

    initial commit

---
+++ week-1/kobyluck-discussion-1.1.js
@@ -0,0 +1,29 @@
+/*======================================
+; Title: Discussion 1.1
+; Author: Jonathan Kobyluck
+; Date; 25 November 2019
+; Modified By: Jonathan Kobyluck
+; Description: Showing a program with 2
+; deliberate issues.
+; ======================================
+*/
+
+// start program
+
+/*
+    Expected output:
+
+    This program has 2 issues
+    AND I FOUND THEM BOTH!
+*/
+
+//variable assignment
+var t = "This program has 2;
+var i = "issues";
+var a = "AND I";
+var f = "FOUND THEM BOTH!";
+
+
+// output
+console.log(t, i);
+console.log(a f);
