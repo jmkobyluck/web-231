417b5611969e82e23219a4f883d7e43c3467f776
Jonathan Koblyuck <jkobyluck@my365.bellevue.edu>
2019-12-01 16:05:45 -0500

    updated saved file

--- week-1/laurie-mallioux-debugging-javascript.js
+++ week-1/laurie-mallioux-debugging-javascript.js
@@ -11,8 +11,3 @@
    var my_str = window.prompt('Enter your name'); //"Enter your name" was missing an ending quote, causing the lines to run together
    var hello = "Hello World";  //added an 'r' to 'va' in order to allow the declaration for "Hello World"
    var x = 5;
-
-//adding output for the previous declarations
-
-console.log(hello);
-//console.log(my_str);
