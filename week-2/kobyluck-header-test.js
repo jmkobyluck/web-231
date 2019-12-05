2cf229ac5cc1e49610b11e4fefef37f4d5d9b942
Jonathan Koblyuck <jkobyluck@my365.bellevue.edu>
2019-12-05 03:13:36 -0500

    Adding week-2 files

---
+++ week-2/kobyluck-header-test.js
@@ -0,0 +1,18 @@
+/*
+============================================
+; Title:  kobyluck-header-test.js
+; Author: Professor Krasso
+; Date:   5 December 2019
+; Modified By: Jonathan Kobyluck
+; Description: Displays a formatted header
+;===========================================
+*/
+
+
+/*
+* Assigning "header" and pulling the "kobyluck-header.js" file
+* Displaying the function in the console log under the define format
+*/
+const header = require('./kobyluck-header.js');
+
+console.log(header.display("Jonathan", "Kobyluck", "Exercise 2.2 - Header"));
