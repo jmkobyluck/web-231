0f64b815282da073452f2e33c9d6d0ad1f72347d
Jonathan Koblyuck <jkobyluck@my365.bellevue.edu>
2020-01-11 13:33:52 -0500

    Submitting Exercise 5.2

---
+++ week-5/kobyluck-exercise-5.2.js
@@ -0,0 +1,45 @@
+/*
+============================================
+; Title:  Kobyluck Exercise 5.2
+; Author: Professor Krasso
+; Date:   9 Janurary 2020
+; Modified By: Jonathan Kobyluck
+; Description: ES5 Built-in Functions
+;===========================================
+*/
+
+// Require statement that imports my header file
+const header = require('../week-2/kobyluck-header.js');
+
+// Calling the console log function to display my header
+console.log(header.display('Johnny', 'Kobyluck', 'Exercise 5.2'));
+
+// Line break
+console.log("");
+
+
+// start program
+
+/*
+  Expected output:
+
+  FirstName LastName
+  Exercise 5.2
+  Today's Date
+
+  Oysters
+  Shrimp
+  Steak
+  Tacos
+  Sushi
+
+*/
+
+// delcaring food variable
+var food = ['Oysters', 'Shrimp', 'Steak', 'Tacos', 'Sushi'];
+
+// passing "foods" function through ES5 "forEach" function output "food" string
+food.forEach(function(foods) {
+  // output
+  console.log(foods);
+});
