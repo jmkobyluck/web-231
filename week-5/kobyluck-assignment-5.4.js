7212bba7cd9f20c2d9d2110c855cf3af74b86c58
Jonathan Koblyuck <jkobyluck@my365.bellevue.edu>
2020-01-12 17:05:31 -0500

    Adding Assignment 5.4

--- week-5/kobyluck-assignment-5.4.js
+++ week-5/kobyluck-assignment-5.4.js
@@ -2,7 +2,7 @@
 ============================================
 ; Title:  Kobyluck Assignment 5.4
 ; Author: Professor Krasso
-; Date:   11 Janurary 2020
+; Date:   12 January 2020
 ; Modified By: Jonathan Kobyluck
 ; Description: Filtering/Reducing
 ; Complex Data Structures
@@ -58,7 +58,10 @@ console.log("");
   Composer: Schubert
 */
 
-var composer = [
+// start program
+
+// declaring my composers array
+var composers = [
   {
     firstName: "Ludwig Van",
     lastName: "Beethoven",
@@ -91,13 +94,39 @@ var composer = [
   }
 ];
 
-var ratings = composer.map(function(composers) {
-  return composers.rating
+// defining "ratings" map function to return the ratings by composer
+var ratings = composers.map(function(composer) {
+  return {
+    rating: composer.rating,
+    composer: composer.lastName
+  };
 });
 
+// defining "genres" map function to return the genres by composer
+var genres = composers.map(function(composer) {
+  return {
+    genre: composer.genre,
+    composer: composer.lastName
+  };
+});
+
+// output
 console.log("-- COMPOSER BY RATING --");
 
-ratings.forEach(function(rating, lastName) {
-  console.log("Rating: " + rating);
-  console.log("Composer: " + lastName);
+// forEach function outputting the "ratings" map
+ratings.forEach(function(composers) {
+  console.log("Rating: " + composers.rating);
+  console.log("Composer: " + composers.composer);
+  console.log("");
 });
+
+console.log("-- COMPOSER BY GENRE --");
+
+// forEach function outputting the "genres" map
+genres.forEach(function(composers) {
+  console.log("Genre: " + composers.genre);
+  console.log("Composer: " + composers.composer);
+  console.log("");
+});
+
+// end program
