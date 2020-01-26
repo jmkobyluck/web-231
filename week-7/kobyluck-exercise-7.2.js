fd2038ca88c847daf6ea946a82e7d2dacb4ce9bd
Jonathan Koblyuck <jkobyluck@my365.bellevue.edu>
2020-01-26 16:53:28 -0500

    Adding Exercise 7.2

---
+++ week-7/kobyluck-exercise-7.2.js
@@ -0,0 +1,60 @@
+/*
+============================================
+; Title:  Kobyluck Exercise 7.2
+; Author: Professor Krasso
+; Date:   20 January 2020
+; Modified By: Jonathan Kobyluck
+; Description: Nested Object Literals
+;===========================================
+*/
+
+// Require statement that imports my header file
+const header = require('../week-2/kobyluck-header.js');
+
+// Calling the console log function to display my header
+console.log(header.display('Johnny', 'Kobyluck', 'Exercise 7.2'));
+
+// Line break
+console.log("");
+
+
+// start program
+
+/*
+  Expected output:
+
+  FirstName LastName
+  Exercise 7.2
+  Today's Date
+
+  1 Thomas Edison Software Engineer
+  2 Benjamin Franklin Programmer
+  3 Nikola Tesla Project Manager
+  4 Charles Babbage Product Manager
+  5 Alexander Bell Business Analyst
+*/
+
+// declaring EmployeeLog array
+var employees = [
+  new EmployeeLog(1, "Thomas", "Edison", "Software Engineer"),
+  new EmployeeLog(2, "Benjamin", "Franklin", "Programmer"),
+  new EmployeeLog(3, "Nikola", "Tesla", "Project Manager"),
+  new EmployeeLog(4, "Charles", "Babbage", "Product Manager"),
+  new EmployeeLog(5, "Alexander", "Bell", "Business Analyst")
+];
+
+// constructor object function
+function EmployeeLog(id, firstName, lastName, title) {
+  this.id = id;
+  this.firstName = firstName;
+  this.lastName = lastName;
+  this.title = title;
+};
+
+// loop to output the data from EmployeeLog array
+employees.forEach(function(employee) {
+ console.log(employee.id + " " +employee.firstName + " " + employee.lastName + " " + employee.title);
+});
+
+
+// end program
